cfd78fdd4c3b777851e3bfec215759fb47178e97
猫捉鱼 <111427585+Tokeii0@users.noreply.github.com>
2024-04-16 19:23:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ MemProcFsDir = r'Tools\MemProcFS\MemProcFS.exe'
 volatility2 = r'Tools\volatility2\vol.exe'
 gimp = r'Tools\gimp\bin\gimp-console-2.10.exe'
 profile  = ['Win7SP1x64', 'Win7SP1x86', 'WinXPSP3x86', 'WinXPSP2x86', 'WinXPSP2x64']
+pythonpath = r'mem_venv\Scripts\python.exe' # 你的python3位置
