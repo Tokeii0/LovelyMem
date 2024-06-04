7661321c85caad8d11c2fe2451633aaa27eac989
猫捉鱼 <111427585+Tokeii0@users.noreply.github.com>
2024-06-04 21:25:14 +0800

    Delete convert directory

--- convert/loadcsv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_csvfile(path):
-    df = pd.read_csv(path, encoding='utf-8')
-    #nan转为""
-    df = df.fillna("")
-    return list(df.columns), df.values.tolist()
-
-#print(load_csvfile(r'M:\forensic\csv\process.csv'))
